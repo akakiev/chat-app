30c2d8625c2dc5ebf7cea402d5ed42f059bb41c8
Serhii Baraban <akakieva@gmail.com>
2023-05-17 15:04:48 +0200

    added uploading images to firebase functionality

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
-  const UserImagePicker({super.key});
+  const UserImagePicker({
+    super.key,
+    required this.onPickImage,
+  });
+
+  final void Function(File pickedImage) onPickImage;
 
   @override
   State<StatefulWidget> createState() {
@@ -27,6 +32,8 @@ class _UserImagePickerState extends State<UserImagePicker> {
     setState(() {
       _pickedImageFile = File(pickedImage.path);
     });
+
+    widget.onPickImage(_pickedImageFile!);
   }
 
   @override
